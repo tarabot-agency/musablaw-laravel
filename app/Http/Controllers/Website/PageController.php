c9a56ddf9cb08901106996c530d3cce9f3a2bed5
tarabot-agency <abobakr.tarabot@gmail.com>
2024-11-24 10:58:55 +0200

    add 5 services in home page

--- app/Http/Controllers/Website/PageController.php
+++ app/Http/Controllers/Website/PageController.php
@@ -46,7 +46,7 @@ class PageController extends Controller
                 'image'
             )
                 ->where('section', 'our_services')
-                ->take(3)
+                ->take(5)
                 ->get()->map(function ($page) {
                     $page->image = $page->image ? asset('images/pages/' . $page->image) : '';
                     return $page;
